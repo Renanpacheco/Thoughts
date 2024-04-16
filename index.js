363d0316cea5f6523e7cf7406c2b80605edbd670
Renanpacheco <prenan86@gmail.com>
2024-04-16 11:24:05 -0300

    initial setup

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const Filestore = require('session-file-store')(session);
+const flash = require('express-flash');
+const conn = require ('./db/conn')
+
+const app = express();
+
+// template engine
+app.engine('handlebars',exphbs.engine())
+app.set('view engine', 'handlebars');
+
+app.use(express.urlencoded({ extended: true }));
+
+// helps receiving json
+app.use(express.json());
+
+//sesion midleware
+//save session in a determinate local
+app.use(
+    session({
+        name: "session",
+        secret: "we_secret",
+        resave: false,
+        saveUninitialized: false,
+        store: new Filestore({
+            logFn: function(){},
+            path: require("path").join(require('os').tmpdir(), "sessions"),
+        }),
+        cookie:{
+            secure: false,
+            maxAge: 360000,
+            expires: new Date(Date.now()+ 360000),
+            httpOnly: true // use only for the localhost
+        }
+
+    }),
+)
+
+// flash messages
+app.use(flash())
+
+//public path
+app.use(express.static('public'))
+
+//set the session to res
+// pass the informations of user to front
+app.use((req, res,next) => {
+
+    if (req.session.userid){
+        res.locals.session = req.session
+    }
+    next()
+})
+
+
+conn.
+sync()
+.then(()=> app.listen(3000))
+.catch(err => console.log(err))
