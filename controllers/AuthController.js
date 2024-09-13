34820cab44c1313ebcd66c53c5221effd6bb81e7
Renanpacheco <prenan86@gmail.com>
2024-09-13 16:49:29 -0300

    finalizatin of register and logout

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,6 @@
+const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+
 module.exports = class ToughtController{
     static login(req, res){
         res.render('auth/login')
@@ -6,4 +9,51 @@ module.exports = class ToughtController{
     static register(req, res){
         res.render('auth/register')
     }
+
+    static async registerPost(req, res){
+        const {name,email,password,confirmpassword} = req.body
+        //password match validation
+        /*if(password !== '' && confirmpassword !== ''){}*/
+
+        if(confirmpassword != password){
+            req.flash('message', 'password mismatch, try again')
+            res.render('auth/register')
+            
+            return
+        }
+        //check if user exits
+        const checIfUserExists = await User.findOne({ where: { email: email}})
+
+        if(checIfUserExists){
+            req.flash('message', 'This e-mail alredy exist')
+            res.render('auth/register')
+            
+            return
+        }
+        // create a password
+        let salt = bcrypt.genSaltSync(10)
+        const hashedPassword = bcrypt.hashSync(password, salt)
+        const user ={
+            name,
+            email,
+            password: hashedPassword
+        }
+        try {
+            const createdUser= await User.create(user)
+            req.flash('message','success registred user')
+            req.session.userid = createdUser.id
+            req.session.save(()=>{
+                res.redirect('/')
+            })
+            
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    static logout(req, res) {
+        req.session.destroy()
+        res.redirect('/login')
+    }
+
 }
